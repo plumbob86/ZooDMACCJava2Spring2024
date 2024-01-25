9bfc92b2fb78b192cdcf2de3f06980d0e0c39d1b
misti <codeine500@hotmail.com>
2024-01-24 22:56:18 -0600

    Git Activity-POJO animal 'Bat' and makeNoise

--- Zoo/src/main/Runner.java
+++ Zoo/src/main/Runner.java
@@ -1,5 +1,6 @@
 package main;
 
+import model.Bat;
 import model.Example;
 
 public class Runner {
@@ -7,11 +8,17 @@ public class Runner {
 	public static void main(String[] args) {
 		Runner run = new Runner();
 		run.go();
+		run.flyingFoxes("Fruit Bat", "Fruit", 33);
 	}
 
 	private void go() {
 		Example example = new Example();
 		example.makeNoise();
 		
+	}
+	
+	private void flyingFoxes(String name, String diet, int wingSize) {
+		Bat bat = new Bat(name, diet, wingSize);
+		System.out.println(bat.makeNoise());
 	}
 }
